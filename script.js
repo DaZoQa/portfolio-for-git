feaf1631c10de45a5c31e8c89de9318aba31f804
DaZoQa <egikvtumane916@gmail.com>
2022-08-02 00:08:21 +0300

    Все блоки окончены

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var aboutBtn = document.querySelector('#about-btn');
-var workBtn = document.querySelector('#work-btn');
-var aboutPage = document.querySelector("#about");
-var workPage = document.querySelector("#work");
+var homeBtn        = document.querySelector('#home-btn');
+var aboutBtn       = document.querySelector('#about-btn');
+var workBtn        = document.querySelector('#work-btn');
+var contactBtn     = document.querySelector('#contact-btn');
+var footHomeBtn    = document.querySelector('#foot-home-btn')
+var footAboutBtn   = document.querySelector('#foot-about-btn');
+var footWorkBtn    = document.querySelector('#foot-work-btn');
+var footContactBtn = document.querySelector('#foot-contact-btn');
+var homePage       = document.querySelector('#home');
+var aboutPage      = document.querySelector("#about");
+var workPage       = document.querySelector("#work");
+var contactPage    = document.querySelector("#contact");
 
+homeBtn.addEventListener('click', () =>{
+  scrollTo(homePage);
+})
 aboutBtn.addEventListener('click', () =>{
   scrollTo(aboutPage);
 })
 workBtn.addEventListener('click', () =>{
   scrollTo(workPage);
+})
+contactBtn.addEventListener('click', () =>{
+  scrollTo(contactPage);
+})
+footHomeBtn.addEventListener('click', () =>{
+  scrollTo(homePage);
+})
+footAboutBtn.addEventListener('click', () =>{
+  scrollTo(aboutPage);
+})
+footWorkBtn.addEventListener('click', () =>{
+  scrollTo(workPage);
+})
+footContactBtn.addEventListener('click', () =>{
+  scrollTo(contactPage);
 })
 
 function scrollTo(element){
